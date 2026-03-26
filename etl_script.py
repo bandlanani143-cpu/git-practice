f67b54d0ab510a758f89464fc8a8f8b3895d7f14
Brahmaiah Bandla <bandlanani143@gmail.com>
2026-03-26 23:29:47 +0530

    Modified Multiple Transformations"

--- etl_script.py
+++ etl_script.py
@@ -1,4 +1,4 @@
-print("Added transformation logic")
+print("Added transformation logic and Modified Logic")
 print("Insertion transformation logic")
 print("update transformation logic")
 print("Delete transformation logic")
